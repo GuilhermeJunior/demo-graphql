1964b6065fb0c6fd626e1f413f9160d80c4ec7bb
Guilherme Chamorro <poltegaisnt@gmail.com>
2024-01-05 14:48:01 -0300

    adicionar jpa e banco em memória

--- src/main/java/dev/stomrgui/demographql/BookController.java
+++ src/main/java/dev/stomrgui/demographql/BookController.java
@@ -1,5 +1,9 @@
 package dev.stomrgui.demographql;
 
+import dev.stomrgui.demographql.model.Author;
+import dev.stomrgui.demographql.model.Book;
+import dev.stomrgui.demographql.model.BookEntity;
+import dev.stomrgui.demographql.repository.BookRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
@@ -8,13 +12,25 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class BookController {
 
-    @QueryMapping
-    public Book bookById(@Argument String id) {
-        return Book.getById(id);
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
-    @SchemaMapping
-    public Author author(Book book) {
-        return Author.getById(book.authorId());
+//    @QueryMapping
+//    public Book bookById(@Argument String id) {
+//        return Book.getById(id);
+//    }
+//
+//    @SchemaMapping
+//    public Author author(Book book) {
+//        return Author.getById(book.authorId());
+//    }
+
+    @QueryMapping
+    public BookEntity bookById(@Argument Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(RuntimeException::new);
     }
 }
---
+++ src/main/java/dev/stomrgui/demographql/model/BookEntity.java
@@ -0,0 +1,37 @@
+package dev.stomrgui.demographql.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Long id;
+
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "author_id")
+    private AuthorEntity author;
+
+}
