aed598c7d420f269313e76059be232b24f94cbf0
Guilherme Chamorro <poltegaisnt@gmail.com>
2024-01-08 20:46:53 -0300

    testes

--- src/test/java/dev/stomrgui/demographql/BookControllerTests.java
+++ src/test/java/dev/stomrgui/demographql/BookControllerTests.java
@@ -1,9 +1,17 @@
 package dev.stomrgui.demographql;
 
+import dev.stomrgui.demographql.model.AuthorEntity;
+import dev.stomrgui.demographql.model.BookEntity;
+import dev.stomrgui.demographql.repository.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.graphql.test.tester.GraphQlTester;
+
+import java.util.Optional;
 
 @GraphQlTest(BookController.class)
 class BookControllerTests {
@@ -11,23 +19,46 @@ class BookControllerTests {
     @Autowired
     private GraphQlTester graphQlTest;
 
+    @MockBean
+    private BookRepository bookRepository;
+
+    @BeforeEach
+    void setUp() {
+        Mockito.when(bookRepository.findById(Mockito.any())).thenReturn(
+                Optional.of(mockBook())
+        );
+    }
+
     @Test
     void shouldGetFirstBook() {
         this.graphQlTest
                 .documentName("bookDetails")
-                .variable("id", "book-1")
+                .variable("id", 1L)
                 .execute()
                 .path("bookById")
                 .matchesJson("""
                     {
-                        "id": "book-1",
+                        "id": "1",
                         "name": "Effective Java",
-                        "pageCount": 416,
                         "author": {
                           "firstName": "Joshua",
                           "lastName": "Bloch"
                         }
                     }
                 """);
+    }
+
+    private BookEntity mockBook() {
+        var author = new AuthorEntity();
+        author.setId(1L);
+        author.setFirstName("Joshua");
+        author.setLastName("Bloch");
+
+        var book = new BookEntity();
+        book.setName("Effective Java");
+        book.setId(1L);
+        book.setAuthor(author);
+
+        return book;
     }
 }
